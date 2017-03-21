dd8bf7ccd52b4c3a29e86ccc8642c880d3f07185
ouhaifan <ou.81@osu.edu>
2017-03-21 17:40:00 -0400

    Initial commit

---
+++ Luxury-radar/src/main/java/haifanou/com/luxuryradar/CamaraIntentActivity.java
@@ -0,0 +1,124 @@
+package haifanou.com.luxuryradar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class CamaraIntentActivity extends Activity implements View.OnClickListener{
+
+    private static final String TAG = "CAM_Activity";
+    private static final int CAM_REQUEST = 0;
+    private static final int GALL_REQUEST = 1;
+
+    Button nextBtn;
+    private ImageView photoImageView;
+    private boolean fromCam=false;
+    private String mImageFileLocation = "";
+    private Uri imageUri;
+    private File photoFile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camara_intent);
+        nextBtn = (Button) findViewById(R.id.nextButton);
+        nextBtn.setEnabled(false);
+        nextBtn.setOnClickListener(this);
+        photoImageView = (ImageView) findViewById(R.id.imageGalleryView);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.nextButton:
+                Intent moreInforIntent = new Intent(this, MoreInforActivity.class);
+                moreInforIntent.putExtra("FromCamOrNot", fromCam);
+                if(fromCam) {
+                    moreInforIntent.putExtra("imageFileLocation", mImageFileLocation);
+                }else{
+                    moreInforIntent.putExtra("imageUri", imageUri.toString());
+                }
+                startActivity(moreInforIntent);
+                break;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_camara_intent, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.btn_camera) takePhoto();
+        else if(item.getItemId()==R.id.btn_gallery) openGallery();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, GALL_REQUEST);
+    }
+
+    public void takePhoto() {
+        Intent callCameraApplicationIntent = new Intent();
+        callCameraApplicationIntent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        try {
+            photoFile = createImageFile();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        callCameraApplicationIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
+
+        startActivityForResult(callCameraApplicationIntent, CAM_REQUEST);
+    }
+
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if(requestCode == CAM_REQUEST && resultCode == RESULT_OK) {
+            fromCam=true;
+            Bitmap photoReducedSizeBitmp = BitmapFactory.decodeFile(mImageFileLocation);
+            photoImageView.setImageBitmap(photoReducedSizeBitmp);
+            nextBtn.setEnabled(true);
+        } else if(requestCode== GALL_REQUEST && resultCode == RESULT_OK) {
+            fromCam = false;
+            imageUri = data.getData();
+            photoImageView.setImageURI(imageUri);
+            nextBtn.setEnabled(true);
+        }
+
+    }
+
+    private File createImageFile() throws IOException {
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "IMAGE_" + timeStamp + "_";
+        File storageDirectory=Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        File image = File.createTempFile(imageFileName,".jpg", storageDirectory);
+        mImageFileLocation = image.getAbsolutePath();
+
+        return image;
+
+    }
+
+
+}
---
+++ Luxury-radar/src/main/java/haifanou/com/luxuryradar/MoreInforActivity.java
@@ -0,0 +1,125 @@
+package haifanou.com.luxuryradar;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MoreInforActivity extends Activity implements View.OnClickListener{
+
+    private final static int CROP_REQUEST=3;
+    private Spinner genderSpinner, styleSpinner;
+    private String mImageFileLocation;
+    private ImageView imageView;
+    private Uri imageUri;
+    private Button btnSubmit, btnCrop;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_infor);
+        imageView=(ImageView) findViewById(R.id.imageGalleryView);
+        if(getIntent().getExtras().getBoolean("FromCamOrNot")) {
+            mImageFileLocation=getIntent().getExtras().getString("imageFileLocation");
+            File file = new File(mImageFileLocation);
+            imageUri=Uri.fromFile(file);
+        }else{
+            imageUri=Uri.parse(getIntent().getExtras().getString("imageUri"));
+        }
+        imageView.setImageURI(imageUri);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        addItemsOnSpinner();
+
+        btnSubmit =(Button) findViewById(R.id.submitBtn);
+        btnSubmit.setOnClickListener(this);
+        btnCrop = (Button) findViewById(R.id.cropBtn);
+        btnCrop.setOnClickListener(this);
+    }
+
+    public void addItemsOnSpinner() {
+
+        genderSpinner = (Spinner) findViewById(R.id.genderSpinner);
+        List<String> list = new ArrayList<>();
+        list.add("men");
+        list.add("women");
+        list.add("men&women");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(    android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(dataAdapter);
+
+        styleSpinner= (Spinner) findViewById(R.id.styleSpinner);
+        List<String> list2 = new ArrayList<>();
+        list2.add("shirt");
+        list2.add("tee");
+        list2.add("jacket");
+        list2.add("coat");
+        list2.add("pants");
+        ArrayAdapter<String> dataAdapter2 = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item, list2);
+        dataAdapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        styleSpinner.setAdapter(dataAdapter2);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.submitBtn:
+
+                break;
+            case R.id.cropBtn:
+                cropImage();
+                break;
+        }
+    }
+
+    private void cropImage(){
+        try{
+            Intent cropIntent=new Intent("com.android.camera.action.CROP");
+            cropIntent.setDataAndType(imageUri, "image/*");
+            cropIntent.putExtra("crop", true);
+            cropIntent.putExtra("outputX", 100);
+            cropIntent.putExtra("outputY", 100);
+            cropIntent.putExtra("scale", true);
+            cropIntent.putExtra("scaleUpIfNeeded", true);
+            cropIntent.putExtra("aspectX", 3);
+            cropIntent.putExtra("aspectY", 4);
+            cropIntent.putExtra("return-data", true);
+
+            startActivityForResult(cropIntent, CROP_REQUEST);
+        }catch (ActivityNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        if(requestCode == CROP_REQUEST && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+        }
+    }
+
+}
